bea309a43579afd2a40a45e504b9ba6540cccff0
anhanh080220@gmail.com <anhanh080220@gmail.com>
2021-05-21 02:27:27 +0700

    thu

---
+++ UTTUniversity/Areas/Admin/Controllers/Tes2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UTTUniversity.Areas.Admin.Controllers
+{
+    public class Tes2Controller : Controller
+    {
+        // GET: Admin/Tes2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
