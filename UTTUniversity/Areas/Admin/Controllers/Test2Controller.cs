c6af156e0369b239cfe7f25a3d62d8bb0a7ec37a
anhanh080220@gmail.com <anhanh080220@gmail.com>
2021-05-21 10:01:16 +0700

    âsa

---
+++ UTTUniversity/Areas/Admin/Controllers/Test2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UTTUniversity.Areas.Admin.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        // GET: Admin/Test2
+        public ActionResult Index()
+        {
+            stringa;
+            return View();
+        }
+    }
+}
