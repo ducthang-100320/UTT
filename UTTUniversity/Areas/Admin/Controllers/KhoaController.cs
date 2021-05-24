197a55d15f0dd8482034eb40df1df6d2b867f80d
ducthang-100320 <ducthang100320@gmail.com>
2021-05-24 17:27:25 +0700

    add login,nhanvien,phongban edit layout

---
+++ UTTUniversity/Areas/Admin/Controllers/KhoaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UTTUniversity.Areas.Admin.Controllers
+{
+    public class KhoaController : Controller
+    {
+        // GET: Admin/Khoa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
