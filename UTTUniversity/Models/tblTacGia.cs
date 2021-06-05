b1fe15263fc5398db0f4e95fb010f67531c7011e
ducthang-100320 <ducthang100320@gmail.com>
2021-06-05 22:13:12 +0700

    fix chuẩn 2

--- UTTUniversity/Models/tblTacGia.cs
+++ UTTUniversity/Models/tblTacGia.cs
@@ -9,40 +9,30 @@ namespace UTTUniversity.Models
     [Table("tblTacGia")]
     public partial class tblTacGia
     {
-        [Key]
-        [Column(Order = 0)]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        
         public int ID { get; set; }
 
-        [Key]
-        [Column(Order = 1)]
+       
         [StringLength(50)]
         public string MA_TACGIA { get; set; }
 
-        [Key]
-        [Column(Order = 2)]
+        
         [StringLength(150)]
         public string TEN_TACGIA { get; set; }
 
-        [Key]
-        [Column(Order = 3)]
+       
         [StringLength(200)]
         public string IMAGE { get; set; }
 
-        [Key]
-        [Column(Order = 4)]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    
         public int GIOI_TINH { get; set; }
 
-        [Key]
-        [Column(Order = 5)]
+       
         public DateTime NGAY_SINH { get; set; }
 
         public string MO_TA { get; set; }
 
-        [Key]
-        [Column(Order = 6)]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
         public int TRANG_THAI { get; set; }
     }
 }
