577bacc67f76a7d6725bf8d083fdb7a4c53f41b1
anhanh080220@gmail.com <anhanh080220@gmail.com>
2021-06-11 14:22:58 +0700

    thuvien2

--- UTTUniversity/Models/tblTacGia.cs
+++ UTTUniversity/Models/tblTacGia.cs
@@ -9,29 +9,25 @@ namespace UTTUniversity.Models
     [Table("tblTacGia")]
     public partial class tblTacGia
     {
-        
         public int ID { get; set; }
 
-       
+        [Required]
         [StringLength(50)]
         public string MA_TACGIA { get; set; }
 
-        
+        [Required]
         [StringLength(150)]
         public string TEN_TACGIA { get; set; }
 
-       
+        [Required]
         [StringLength(200)]
         public string IMAGE { get; set; }
 
-    
         public int GIOI_TINH { get; set; }
 
-       
         public DateTime NGAYSINH { get; set; }
 
         public string MO_TA { get; set; }
-
 
         public int TRANG_THAI { get; set; }
     }
